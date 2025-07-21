2e7b1c1cd701567959d2f6a7cd8f0c305a1a4026
Erik Rogers <ewrogers@gmail.com>
2025-07-20 23:35:00 -0400

    feat: add basic theme setup

---
+++ Arbiter.App/Controls/TalgoniteWindow.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Avalonia.Controls;
+
+namespace Arbiter.App.Controls;
+
+public class TalgoniteWindow : Window
+{
+    
+    protected override Type StyleKeyOverride { get; } = typeof(Window);
+}
