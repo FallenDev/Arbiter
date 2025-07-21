f81af5da4c485e7910b3e17d64907e356cbbc8df
Erik Rogers <ewrogers@gmail.com>
2025-07-21 01:59:38 -0400

    feat: add title bar functionality

--- Arbiter.App/Controls/TalgoniteWindow.cs
+++ Arbiter.App/Controls/TalgoniteWindow.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 
 namespace Arbiter.App.Controls;
 
 public class TalgoniteWindow : Window
 {
+    private Grid _titleBar;
+    private Button? _minimizeButton;
+    private Button? _maximizeButton;
+    private Button? _closeButton;
+    
     protected override Type StyleKeyOverride { get; } = typeof(TalgoniteWindow);
     
     public static readonly StyledProperty<Control> TitleBarContentProperty = AvaloniaProperty.Register<TalgoniteWindow, Control>(
@@ -25,5 +31,27 @@ public class TalgoniteWindow : Window
         {
             PseudoClasses.Set(":maximized", change.NewValue is WindowState.Maximized);
         }
+    }
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+
+        _titleBar = e.NameScope.Find<Grid>("PART_TitleBar")!;
+        _titleBar.DoubleTapped += (_, _) => ToggleMaximizedState();
+        
+        _minimizeButton = e.NameScope.Find<Button>("PART_MinimizeButton")!;
+        _minimizeButton.Click += (_, _) => WindowState = WindowState.Minimized;
+        
+        _maximizeButton = e.NameScope.Find<Button>("PART_MaximizeButton")!;
+        _maximizeButton.Click += (_, _) => ToggleMaximizedState();
+        
+        _closeButton = e.NameScope.Find<Button>("PART_CloseButton")!;
+        _closeButton.Click += (_, _) => Close();
+    }
+
+    private void ToggleMaximizedState()
+    {
+        WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
     }
 }
