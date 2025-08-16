9826bfc9782cc494228784bf20f5e029cb40c909
Erik Rogers <ewrogers@gmail.com>
2025-08-16 15:39:51 -0400

    feat: add props to inspector packet vm

--- Arbiter.App/ViewModels/InspectorPacketViewModel.cs
+++ Arbiter.App/ViewModels/InspectorPacketViewModel.cs
@@ -1,6 +1,15 @@
-﻿namespace Arbiter.App.ViewModels;
+﻿using System.Collections.ObjectModel;
+using Arbiter.App.Models;
+
+namespace Arbiter.App.ViewModels;
 
 public partial class InspectorPacketViewModel : ViewModelBase
 {
-    
+    public string DisplayName { get; set; } = string.Empty;
+    public PacketDirection Direction { get; set; }
+    public byte Command { get; set; }
+
+    public ObservableCollection<InspectorSectionViewModel> Sections { get; } = [];
+
+    public ObservableCollection<InspectorItemViewModel> Items { get; } = [];
 }
