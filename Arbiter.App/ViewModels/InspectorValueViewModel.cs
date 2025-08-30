331f1c2141c7569defb4dbb0d81a30c98385aabf
Erik Rogers <ewrogers@gmail.com>
2025-08-30 03:13:09 -0400

    feat: toggle hex display

--- Arbiter.App/ViewModels/InspectorValueViewModel.cs
+++ Arbiter.App/ViewModels/InspectorValueViewModel.cs
@@ -4,13 +4,31 @@ namespace Arbiter.App.ViewModels;
 
 public partial class InspectorValueViewModel : InspectorItemViewModel
 {
-    [ObservableProperty] [NotifyPropertyChangedFor(nameof(FormattedValue))]
+    private bool _showHex;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(FormattedValue))]
+    [NotifyPropertyChangedFor(nameof(IsInteger))]
     private object? _value;
 
     [ObservableProperty] [NotifyPropertyChangedFor(nameof(FormattedValue))]
     private string? _stringFormat;
 
-    public string FormattedValue => string.Format(StringFormat ?? "{0}", Value);
+    public bool IsInteger => Value is sbyte or byte or short or ushort or int or uint or long or ulong;
+
+    public bool ShowHex
+    {
+        get => _showHex;
+        set
+        {
+            if (SetProperty(ref _showHex, value))
+            {
+                OnPropertyChanged(nameof(FormattedValue));
+            }
+        }
+    }
+
+    public string FormattedValue => string.Format(ShowHex && IsInteger ? "0x{0:X}" : StringFormat ?? "{0}", Value);
 
     protected override string GetCopyableValue() => FormattedValue;
 }
