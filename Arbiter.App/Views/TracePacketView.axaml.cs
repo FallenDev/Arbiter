85a961e918ad49ce58d3f90a870c753b2c04f3c2
Erik Rogers <ewrogers@gmail.com>
2025-08-29 22:44:51 -0400

    refactor: move track packet to views

---
+++ Arbiter.App/Views/TracePacketView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace Arbiter.App.Views;
+
+public partial class TracePacketView : UserControl
+{
+    public TracePacketView()
+    {
+        InitializeComponent();
+    }
+}
