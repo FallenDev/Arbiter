e30939b650f3f457035cb5cfb5cadfbfa30b8a87
Erik Rogers <ewrogers@gmail.com>
2025-08-29 22:38:49 -0400

    refactor: move console log entry to views

---
+++ Arbiter.App/Views/ConsoleLogEntryView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace Arbiter.App.Views;
+
+public partial class ConsoleLogEntryView : UserControl
+{
+    public ConsoleLogEntryView()
+    {
+        InitializeComponent();
+    }
+}
