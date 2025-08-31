39ee40c389663d9758405bb02e6d62c50009f619
Erik Rogers <ewrogers@gmail.com>
2025-08-31 03:20:39 -0400

    feat: add list view model display

---
+++ Arbiter.App/Views/InspectorListView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace Arbiter.App.Views;
+
+public class InspectorListView : UserControl
+{
+    public InspectorListView()
+    {
+        InitializeComponent();
+    }
+}
