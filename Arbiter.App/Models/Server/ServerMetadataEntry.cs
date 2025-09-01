3ae7d2812a535621c3975707d0f84e95a0646dab
Erik Rogers <ewrogers@gmail.com>
2025-09-01 01:01:12 -0400

    feat: metadata types

---
+++ Arbiter.App/Models/Server/ServerMetadataEntry.cs
@@ -0,0 +1,9 @@
+﻿using Arbiter.App.Annotations;
+
+namespace Arbiter.App.Models.Server;
+
+public class ServerMetadataEntry
+{
+    [InspectProperty] public string Name { get; set; } = string.Empty;
+    [InspectProperty(ShowHex = true)] public uint Checksum { get; set; }
+}
---
+++ Arbiter.App/Models/Server/ServerMetadataResponseType.cs
@@ -0,0 +1,7 @@
+﻿namespace Arbiter.App.Models.Server;
+
+public enum ServerMetadataResponseType
+{
+    Metadata = 0,
+    Listing = 1
+}
