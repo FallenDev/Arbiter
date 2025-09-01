aa0cebd69d3db6c8509ce43a891256551c9a6bf9
Erik Rogers <ewrogers@gmail.com>
2025-09-01 03:46:28 -0400

    feat: move/turn packets

---
+++ Arbiter.App/Models/Server/Messages/ServerEntityTurnMessage.cs
@@ -0,0 +1,24 @@
+﻿using Arbiter.App.Annotations;
+using Arbiter.App.Models.Game;
+using Arbiter.Net;
+using Arbiter.Net.Server;
+
+namespace Arbiter.App.Models.Server.Messages;
+
+[InspectPacket(ServerCommand.EntityTurn)]
+public class ServerEntityTurnMessage : IPacketMessage
+{
+    [InspectSection("Entity")]
+    [InspectProperty(ShowHex = true)]
+    public uint EntityId { get; set; }
+    
+    [InspectSection("Facing")]
+    [InspectProperty]
+    public WorldDirection Direction { get; set; }
+
+    public void ReadFrom(NetworkPacketReader reader)
+    {
+        EntityId = reader.ReadUInt32();
+        Direction = (WorldDirection)reader.ReadByte();
+    }
+}
---
+++ Arbiter.App/Models/Server/Messages/ServerEntityWalkMessage.cs
@@ -0,0 +1,38 @@
+﻿using Arbiter.App.Annotations;
+using Arbiter.App.Models.Game;
+using Arbiter.Net;
+using Arbiter.Net.Server;
+
+namespace Arbiter.App.Models.Server.Messages;
+
+[InspectPacket(ServerCommand.EntityWalk)]
+public class ServerEntityWalkMessage : IPacketMessage
+{
+    [InspectSection("Entity")]
+    [InspectProperty(ShowHex = true)]
+    public uint EntityId { get; set; }
+    
+    [InspectSection("Walk")]
+    [InspectProperty]
+    public WorldDirection Direction { get; set; }
+    
+    [InspectSection("Location")]
+    [InspectProperty]
+    public ushort PreviousX { get; set; }
+    
+    [InspectProperty]
+    public ushort PreviousY { get; set; }
+    
+    [InspectSection("Uncategorized")]
+    [InspectProperty(ShowHex = true)]
+    public byte Unknown { get; set; }
+
+    public void ReadFrom(NetworkPacketReader reader)
+    {
+        EntityId = reader.ReadUInt32();
+        PreviousX = reader.ReadUInt16();
+        PreviousY = reader.ReadUInt16();
+        Direction = (WorldDirection)reader.ReadByte();
+        Unknown = reader.ReadByte();
+    }
+}
