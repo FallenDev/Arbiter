4387d09106413d9f75bd703a7d725362a7fc9a4e
Erik Rogers <ewrogers@gmail.com>
2025-08-31 23:45:50 -0400

    fix: modifiers

--- Arbiter.App/Models/Game/ElementModifier.cs
+++ Arbiter.App/Models/Game/ElementModifier.cs
@@ -2,5 +2,14 @@
 
 public enum ElementModifier : byte
 {
-    
+    None = 0,
+    Fire = 1,
+    Water = 2,
+    Wind = 3,
+    Earth = 4,
+    Light = 5,
+    Dark = 6,
+    Wood = 7,
+    Metal = 8,
+    Undead = 9
 }
