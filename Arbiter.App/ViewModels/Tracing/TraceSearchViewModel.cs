ee2a8110be954b001229753a8946589c9950635c
Erik Rogers <ewrogers@gmail.com>
2025-09-17 23:04:46 -0400

    feat: command filter bindings

--- Arbiter.App/ViewModels/Tracing/TraceSearchViewModel.cs
+++ Arbiter.App/ViewModels/Tracing/TraceSearchViewModel.cs
@@ -1,22 +1,41 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Globalization;
+using Avalonia.Data;
 using CommunityToolkit.Mvvm.Input;
 
 namespace Arbiter.App.ViewModels.Tracing;
 
 public partial class TraceSearchViewModel : ViewModelBase
 {
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(CommandFilter))]
     private byte? _command;
+    private string _commandFilter = string.Empty;
+
+    public byte? Command => _command;
 
     public string? CommandFilter
     {
-        get => Command?.ToString("X2");
+        get => _commandFilter;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                SetProperty(ref _commandFilter, string.Empty);
+                _command = null;
+                return;
+            }
+
+            if (!byte.TryParse(value, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out var command))
+            {
+                throw new DataValidationException("Invalid command filter");
+            }
+
+            SetProperty(ref _commandFilter, value);
+            SetProperty(ref _command, command);
+        }
     }
-    
+
     [RelayCommand]
-    public void ClearCommandFilter()
+    private void ClearCommandFilter()
     {
-        Command = null;
+        CommandFilter = string.Empty;
     }
 }
