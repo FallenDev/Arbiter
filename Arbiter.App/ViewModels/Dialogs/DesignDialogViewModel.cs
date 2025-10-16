027c8a67090790d7cb55c07f8b8baf914f1b40ad
Erik Rogers <ewrogers@gmail.com>
2025-10-16 18:27:19 -0400

    feat: dialog layout

---
+++ Arbiter.App/ViewModels/Dialogs/DesignDialogViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Arbiter.App.ViewModels.Dialogs;
+
+// This is only used at design-time to make it easier to adjust XAML view layout.
+internal class DesignDialogViewModel : DialogViewModel
+{
+    public DesignDialogViewModel()
+    {
+        Name = "Dialog";
+        Content =
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    }
+}
--- Arbiter.App/ViewModels/Dialogs/DialogViewModel.cs
+++ Arbiter.App/ViewModels/Dialogs/DialogViewModel.cs
@@ -1,6 +1,67 @@
-﻿namespace Arbiter.App.ViewModels.Dialogs;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Arbiter.App.ViewModels.Dialogs;
 
 public partial class DialogViewModel : ViewModelBase
 {
+    [ObservableProperty] private string? _name;
+    [ObservableProperty] private string? _content;
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(NavigatePreviousCommand))]
+    private bool _canNavigatePrevious;
     
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(NavigateNextCommand))]
+    private bool _canNavigateNext;
+    
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(NavigateTopCommand))]
+    private bool _canNavigateTop;
+    
+    [RelayCommand(CanExecute = nameof(CanNavigatePrevious))]
+    private void NavigatePrevious()
+    {
+        if (!CanNavigatePrevious)
+        {
+            return;
+        }
+    }
+
+    [RelayCommand(CanExecute = nameof(CanNavigateNext))]
+    private void NavigateNext()
+    {
+        if (!CanNavigateNext)
+        {
+            return;
+        }   
+    }
+
+    [RelayCommand(CanExecute = nameof(CanNavigateTop))]
+    private void NavigateTop()
+    {
+        if (!CanNavigateTop)
+        {
+            return;
+        }
+    }
+    
+    [RelayCommand]
+    private void CloseDialog()
+    {
+        
+    }
+    
+    [RelayCommand]
+    private void LoadDialog()
+    {
+        
+    }
+
+    [RelayCommand]
+    private void SaveDialog()
+    {
+        
+    }
 }
