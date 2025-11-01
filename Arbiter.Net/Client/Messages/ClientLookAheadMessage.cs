54bac6becaede9dd35aae2ce765698af3741819f
Erik Rogers <ewrogers@gmail.com>
2025-10-31 23:31:28 -0400

    feat: look ahead packet

---
+++ Arbiter.Net/Client/Messages/ClientLookAheadMessage.cs
@@ -0,0 +1,9 @@
+﻿using Arbiter.Net.Annotations;
+
+namespace Arbiter.Net.Client.Messages;
+
+[NetworkCommand(ClientCommand.LookAhead)]
+public class ClientLookAheadMessage : ClientMessage
+{
+    // No additional data
+}
